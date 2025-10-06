adfe9c30cb56757528b8ad9ad3baf0757df26315
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-06 20:05:36 +0000

    Mon, Oct 6, 2025, 1:05 PM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -29,15 +29,11 @@ def change_correct_words(correct_words, current_words):
     """
     # print(correct_words, current_words)
 
-    for i in range(len(current_words)):  # i goes from 0 to len(current_words)-1
+    for i in range(len(current_words)):  
         if current_words[i] in correct_words:
             current_words[i] = BLANK  # replace with "-----"
 
     return current_words
-
-            # change element at index in    list
-        #     if word in correct_words:
-
 
 
 
