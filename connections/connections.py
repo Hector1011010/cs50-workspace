ce67e408dafcc4a027c60c16472d4e19bd1b3a8e
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-06 16:17:04 +0000

    Mon, Oct 6, 2025, 9:17 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -22,9 +22,8 @@ def main():
 # Write your functions here!
 def change_correct_words(correct_words, current_words):
     """
-    >>> change_correct_words(["AIR", "RUN", "SCREEN"], ["AIR", "RUN", "SCREEN", "REWIND", "SHUFFLE", "SKIP","BATH", "CARD", "PICTURE"])
-    ["-----", "-----", "-----",
-                     "REWIND", "SHUFFLE", "SKIP",
+    >>> change_correct_words(["AIR", "RUN", "SCREEN"], ["AIR", "RUN", "SCREEN", "REWIND", "SHUFFLE", "SKIP", "BATH", "CARD", "PICTURE"])
+    ["-----", "-----", "-----", "REWIND", "SHUFFLE", "SKIP",
                      "BATH", "CARD", "PICTURE"]
     """
     print(correct_words, current_words)
