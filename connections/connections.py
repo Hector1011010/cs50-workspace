1e7515527f3cdb51145e255cd6c8e5c93cb98bd7
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-06 16:53:09 +0000

    Mon, Oct 6, 2025, 9:53 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -30,7 +30,7 @@ def change_correct_words(correct_words, current_words):
     # print(correct_words, current_words)
 
     for first_word in correct_words:
-        
+
         for word in correct_words:
             print(correct_words)
         #     if word in correct_words:
