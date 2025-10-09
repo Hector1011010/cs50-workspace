243aa2a86093e60040d6b2e32115ea61bdf78fe9
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-09 23:13:25 +0000

    Thu, Oct 9, 2025, 4:13 PM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -28,6 +28,8 @@ def change_correct_words(correct_words, current_words):
     ['-----', '-----', '-----', 'REWIND', 'SHUFFLE', 'SKIP', 'BATH', 'CARD', 'PICTURE']
     >>> change_correct_words(["REWIND", "SHUFFLE", "SKIP"], ["AIR", "RUN", "SCREEN", "REWIND", "SHUFFLE", "SKIP", "BATH", "CARD", "PICTURE"])
     ['AIR', 'RUN', 'SCREEN', '-----', '-----', '-----', 'BATH', 'CARD', 'PICTURE']
+    >>> change_correct_words(["REWIND", "SHUFFLE", "SKIP"], ["AIR", "RUN", "SCREEN", "REWIND", "SHUFFLE", "SKIP", "BATH", "CARD", "PICTURE"])
+    ['AIR', 'RUN', 'SCREEN', '-----', '-----', '-----', 'BATH', 'CARD', 'PICTURE']
     """
     for i in range(len(current_words)):
         if current_words[i] in correct_words:
