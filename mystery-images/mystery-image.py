150415adfd3c8c979e23f6d759473b19e77618e1
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-21 17:19:20 +0000

    Tue, Oct 21, 2025, 10:19 AM -07:00

--- mystery-images/mystery-image.py
+++ mystery-images/mystery-image.py
@@ -18,7 +18,7 @@ def create_and_set_pixels(filename):
 
     pixels[4][3] = (255, 255, 0)
 
-    pixels[5][1] = (0, 0, 0)
+    pixels[5][2] = (0, 0, 255)
 
     # First, draw out on your piece of paper what you think this
     # image will look like!
