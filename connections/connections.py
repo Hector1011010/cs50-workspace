e44dfeb4fe6cc384705e79e65d9f236e2a860c72
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-22 18:38:59 +0000

    Wed, Oct 22, 2025, 11:38 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -13,7 +13,9 @@ all_groups = [group1, group2, group3]
 
 def main():
     print("Welcome to Connections!")
-    shuffle()
+    shuffle("AIR", "RUN", "SCREEN",
+                     "REWIND", "SHUFFLE", "SKIP",
+                     "BATH", "CARD", "PICTURE")
     print_grid(3, ["AIR", "RUN", "SCREEN", "REWIND", "SHUFFLE", "SKIP", "BATH", "CARD", "PICTURE"])
 
 
