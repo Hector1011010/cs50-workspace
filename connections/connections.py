0c95aa75a0eaae41bf449e13dccf7d03c6da989a
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-22 18:45:30 +0000

    Wed, Oct 22, 2025, 11:45 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -17,7 +17,7 @@ current_words = ["AIR", "RUN", "SCREEN",
 
 def main():
     print("Welcome to Connections!")
-    print_grid(3, [current_words])
+    print_grid(3, current_words)
     get_user_guess()
 
 
