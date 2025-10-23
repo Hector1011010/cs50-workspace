3930763a206397f2ee6eebfafbb373159f9a8244
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-23 18:44:50 +0000

    Thu, Oct 23, 2025, 11:44 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -111,7 +111,7 @@ def sort_answer(game_answer):
 
 def inputscorrect(inputs, group1, group2, group3):
    # does my inputs match groups[0]?
-   if inputs == group1:
+   if correct == group1:
        return True
    # does my inputs match groups[1]?
    elif inputs == group2:
