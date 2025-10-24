6556b020f497790d1c1780b5244019accd89f857
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-24 18:51:22 +0000

    Fri, Oct 24, 2025, 11:51 AM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -144,7 +144,7 @@ def check_win(words: list[str]) -> bool:
 def main():
     shuffled_words = shuffle(current_words)
     print("Welcome to Connections!")
-    print_grid(5, shuffled_words)
+    print_grid(4, shuffled_words)
 
     while True:
         user_guess = get_user_guess()
