669fd1320d118ffbe49e2b2fe4f95a6abb3fcf96
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-25 21:41:41 +0000

    Sat, Oct 25, 2025, 2:41 PM -07:00

--- connections/connections.py
+++ connections/connections.py
@@ -6,11 +6,7 @@ BLANK = "-----"
 # Declare the word groups
 group1 = [Bark
 
-Pitch
-
-Note
-
-Scale"]
+Pitch", "Note", "Scale"]
 group2 = ["DAY", "SPOT", "GAS", "HEAD"]
 group3 = ["HARD", "RECORD", "SILENCE", "KEY"]
 group4 = ["JUDGE", "TENNIS", "APPEAL", "CASE"]
