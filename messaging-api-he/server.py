8ec3c9bf309bd0340c47b63681d8a5167cd82127
Hector1011010 <231058673+Hector1011010@users.noreply.github.com>
2025-10-30 18:36:06 +0000

    Thu, Oct 30, 2025, 11:36 AM -07:00

---
+++ messaging-api-he/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+# Add other endpoints here!
+
+
+# Leave this at the bottom of your code
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3001)
